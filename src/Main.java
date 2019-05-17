1891acf765d664858eafb304ad8b57ee50613598
Jeremy Duong <duo13001@byui.edu>
2019-05-17 08:40:56 -0600

    Jeremy: added my message

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args){
         System.out.println("Hello, my name is Mason");
         System.out.println("Hello Mason, my name is Austin");
+        System.out.println("Hello Everyone, I'm Jeremy!");
     }
 }
